d97971aee628d4bbab4ba96b37f3ad9bbedd37cd
vigarcia <vigarcia828@gmail.com>
2024-04-02 13:10:02 -0700

    newstuff

--- webapp.py
+++ webapp.py
@@ -16,10 +16,12 @@ def home():
 def render_fact():
     states = get_state_options()
     state = request.args.get('state')
-    county = county_most_under_18(state)
-    fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
-    return render_template('home.html', state_options=states, funFact=fact)
+    county1 = county_most_under_18(state)
+    county2 = highest_pop_final(state)
+    fact1 = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county1 + "."
+    fact2 = "In " + state + ", the county with the highest population is " + county2 + "."
     
+    return render_template('home.html', state_options=states, funFact=fact1, factTwo=fact2)
 def get_state_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
     with open('demographics.json') as demographics_data:
@@ -32,6 +34,20 @@ def get_state_options():
     for s in states:
         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
+    
+def highest_pop_final(state):
+    """returns county with highest population"""
+    with open('demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    highest = 0
+    count = ""
+    
+    for number in counties: 
+        if number["State"] == state:
+            if number["Population"]["2014 Population"] > highest:
+                highest = number["Population"]["2014 Population"]
+                count = number["County"] + ", " + number["State"]
+    return count
 
 def county_most_under_18(state):
     """Return the name of a county in the given state with the highest percent of under 18 year olds."""
